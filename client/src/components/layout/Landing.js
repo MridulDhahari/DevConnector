e89297b87739fc844bfd3404912c6ec13eb1f575
Mridul Dhahari <mridul.dhari141010@gmail.com>
2023-10-25 20:46:10 +0530

    updated version

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,40 +1,4 @@
-// import React from 'react'
-// import {Link,Navigate} from 'react-router-dom';
-// import {connect} from 'react-redux';
-// import PropTypes from 'prop-types';
-// const Landing = ({isAuthenticated}) => {
-//   if(isAuthenticated){
-//     return <Navigate to='/dashboard' />
-//   }
 
-//   return (
-//     <section className="landing">
-//       <div className="dark-overlay">
-//         <div className="landing-inner">
-//           <h1 className="x-large">GeekHub</h1>
-//           <p className="lead">
-//             Create a developer profile/portfolio, share posts and get help from
-//             other developers
-//           </p>
-//           <div className="buttons">
-//             <Link to='/register' className="btn btn-primary">Sign Up</Link>
-//             <Link to='/login' className="btn btn-light">Login</Link>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   )
-// }
-
-// Landing.propTypes = {
-//   isAuthenticated : PropTypes.bool,
-// }
-
-// const mapStateToProps = state => ({
-//   isAuthenticated: state.auth.isAuthenticated
-// })
-
-// export default connect(mapStateToProps)(Landing)
 
 import React from 'react';
 import { Link, Navigate } from 'react-router-dom';
@@ -50,7 +14,7 @@ const Landing = ({ isAuthenticated }) => {
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
-          <h1 className="x-large">Developer Connector</h1>
+          <h1 className="x-large">Bringing Developer Together</h1>
           <p className="lead">
             Create a developer profile/portfolio, share posts and get help from
             other developers
